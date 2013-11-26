f9b5ec3d7439abc66ff3bffee2f7cda7fb9ee7ac
lincoln1 <lincoln.mtl@gmail.com>
2013-11-25 21:39:14 -0500

    Added CreateCrew and listCrew, however setLeader missing
    Added CreateCrew and listCrew, however setLeader is not defined yet.

--- src/reservationsystem/client/ConcordiaAirlineClient.java
+++ src/reservationsystem/client/ConcordiaAirlineClient.java
@@ -11,10 +11,15 @@ import java.util.Map;
 import jline.ArgumentCompletor;
 import jline.ConsoleReader;
 import jline.SimpleCompletor;
+import reservationsystem.GeneralFlight;
 import reservationsystem.impl.AirportImpl;
+import reservationsystem.impl.CrewImpl;
 import reservationsystem.impl.GeneralFlightImpl;
+import reservationsystem.impl.PassengerImpl;
+import reservationsystem.impl.PersonImpl;
 import reservationsystem.impl.UserImpl;
 
+@SuppressWarnings("null")
 public class ConcordiaAirlineClient {
     private static PrintWriter out;
     private static ConsoleReader reader;
@@ -72,6 +77,25 @@ public class ConcordiaAirlineClient {
         generalFlights.put(tmp.getFlightNo(), tmp);
     }
 
+    private static final Map<String, CrewImpl> crews;
+    static {
+    	crews = new HashMap<String, CrewImpl>();
+    	CrewImpl tmp = null;
+    	tmp.setEmployeeId("007");
+    	tmp.setName("Nanda");
+    	tmp.setMiddleName("Li");
+    	tmp.setFamilyName("Jun");
+    	tmp.setResidence("Canada");
+    	tmp.setAddr("7141 Sherbrooke St.");
+    	tmp.setEmail("deng.lijun@encs.concordia.ca");
+    	tmp.setGender(0);
+    	tmp.setBirthDate(null);
+    	tmp.setId(0);
+    	tmp.setPassportId(null);
+    	tmp.setPhone(null);
+    	tmp.setCitizenship("Canadian");
+    	crews.put(tmp.getEmployeeId(),tmp);
+    }
     public static void main(String[] args) throws IOException {
 
         reader = new ConsoleReader();
@@ -202,6 +226,87 @@ public class ConcordiaAirlineClient {
 
     }
 
+    private static void listCrew() throws Exception {
+        for (CrewImpl crew : crews.values()) {
+        	out.println("EmployeeID " + crew.getEmployeeId());
+        	out.println("Name " + crew.getName());
+        	out.println("Middle Name " + crew.getMiddleName());
+        	out.println("Family Name " + crew.getFamilyName());
+        	out.println("Residence " + crew.getResidence());
+        	out.println("Address " + crew.getAddr());
+        	out.println("e-mail " + crew.getEmail());
+        	out.println("Gender" + crew.getGender());
+        	out.println("Birth Date" + crew.getBirthDate());
+        	out.println("ID" + crew.getId());
+        	out.println("Passport number "+ crew.getPassportId());
+        	out.println("Phone Number" + crew.getPhone());
+        	out.println("Citizenship " + crew.getCitizenship());
+            out.println("");
+        }
+        out.flush();
+        return;
+
+    }
+
+    private static void createCrew() throws Exception {
+        if (loginUser.getUserType() != RealUserType.OPERATOR.value) {
+            out.println("illegal command for your user type!");
+            out.flush();
+            return;
+        }
+        String tmp = reader.readLine("Enter the filght No.: ");
+        if (tmp != null && !tmp.equals("")) {
+
+            GeneralFlightImpl gflight = generalFlights.get(tmp);
+            if (gflight != null) {
+                out.println("The flight No. " + tmp + " already exsit!");
+                out.flush();
+                return;
+            }
+            gflight = new GeneralFlightImpl();
+            gflight.setFlightNo(tmp);
+            tmp = reader.readLine("Enter the airport code of departure: ");
+            if (tmp != null && !tmp.equals("")) {
+                AirportImpl ap = airports.get(tmp);
+                if (ap == null) {
+                    out.println("The airport " + tmp + " doesn't exsit!");
+                    out.flush();
+                    return;
+                }
+                gflight.setFrom(ap);
+
+                tmp = reader.readLine("Enter the airport code of destination: ");
+                if (tmp != null && !tmp.equals("")) {
+                    AirportImpl apDest = airports.get(tmp);
+                    if (apDest == null) {
+                        out.println("The airport " + tmp + " doesn't exsit!");
+                        out.flush();
+                        return;
+                    }
+                    gflight.setTo(apDest);
+                    tmp = reader.readLine("Enter the departure time: ");
+                    if (tmp != null && !tmp.equals("")) {
+                        gflight.setDepartureTime(tmp);
+
+                        tmp = reader.readLine("Enter the arrival time: ");
+                        if (tmp != null && !tmp.equals("")) {
+                            gflight.setArrivalTime(tmp);
+                            generalFlights.put(gflight.getFlightNo(), gflight);
+                            out.println("Add general flight success!");
+                            out.flush();
+                            return;
+                        }
+                    }
+
+                }
+
+            }
+        }
+        out.println("error! quit!");
+        out.flush();
+        return;
+
+    }
     private static void printWelcomePage() {
         out.println("Welcome to Concordia Airline Reservation System!");
         out.println("Powered by SSS");
